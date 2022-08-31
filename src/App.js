a75e17bec2a8c2a0e44ff8cb8f247dbfbbb6cfe7
Michias Shiferaw <mshif073@uottawa.ca>
2022-08-31 17:05:40 -0400

    Personal Website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Sidebar from './Components/Sidebar1/Sidebar';
+import { ProfileInfo } from './Components/Sidebar1/SidebarData';
+import logo from './Portrait1.png';
+import About from './Components/About/About';
+import Experience from './Components/Experience/Experience';
+
+import Project from './Components/Project/Project';
+import Footer from './Components/Footer/Footer';
+import Contact from './Components/Contact/Contact';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>H2</h1>
-      </header>
+      <Sidebar>
+        <div className="container mt-5" id="Home">
+          <img
+            src={`https://MichiasShiferaw.github.io/IMG/Pacman/pacman4.png`}
+            id="yourdiv1"
+          />
+          <img
+            src={`https://MichiasShiferaw.github.io/IMG/Pacman/pacman3.png`}
+            id="yourdiv2"
+          />
+          <img
+            src={`https://MichiasShiferaw.github.io/IMG/Pacman/pacman1.png`}
+            id="yourdiv3"
+          />
+
+          <img
+            src={`https://MichiasShiferaw.github.io/IMG/Pacman/pacman2.png`}
+            id="yourdiv4"
+          />
+          <div className="row justify-center">
+            <div className="col-12-xs col-9-md">
+              <p className="text-gray mb-2"> Hi</p>
+
+              <h2>
+                <div className="font-xxl text-white">Michias Shiferaw</div>
+                <div style={{ color: " #3f51b5" }} className="font-xl">
+                  Developer
+                </div>
+              </h2>
+              <p className="text-gray mt-2 mb-3">Welcome to my Website!</p>
+              <a
+                href="#Project"
+                className="btn-outlined-secondary text-secondary text-hover-white"
+              >
+                <span className="display-b">
+                  View My Work<i className="h2 bi bi-caret-right-fill"></i>
+                </span>
+              </a>
+            </div>
+            <div id="mylogo1" className="col-12-xs col- col-3-md ">
+              <img src={logo} alt="Logo" />
+            </div>
+          </div>
+        </div>
+
+        <About />
+        <Experience />
+        <Project />
+
+        <Contact />
+
+        <Footer />
+      </Sidebar>
     </div>
   );
 }
---
+++ src/Components/Project/ProjectData.js
@@ -0,0 +1,32 @@
+export const ProjectData = [
+  {
+    title: "Animated Website",
+    tools: ["Sass", "React", "Firebase"],
+    link: "https://michiasshiferaw.github.io/",
+  },
+  {
+    title: "Personal Website",
+    tools: ["Bootstrap", "React", "Firebase"],
+    link: "https://porto22.netlify.app/",
+  },
+  {
+    title: "Face Your Food",
+    tools: ["Bootstrap", "React", "Firebase"],
+    link: "https://faceyourfood.netlify.app/",
+  },
+  {
+    title: "CarDoctor",
+    tools: ["Bootstrap", "React"],
+    link: "https://carrdoctor.netlify.app/",
+  },
+  {
+    title: "Animated Game",
+    tools: ["Vanillajs"],
+    link: "/code",
+  },
+  {
+    title: "Costco Management",
+    tools: ["C++"],
+    link: "/code",
+  },
+];
